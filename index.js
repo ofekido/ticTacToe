9174a38deb80bffc20392e31066eebf2a705eb4c
Ido <ofekidodo@gmail.com>
2023-12-07 21:00:25 +0200

    added our pictures as "X" and "O"

--- index.js
+++ index.js
@@ -1,76 +1,92 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cells = document.querySelectorAll(".cell");
-    const restartButton = document.querySelector(".btnRestart");
-  
-    let currentPlayer = "X";
-    let gameBoard = ["", "", "", "", "", "", "", "", ""];
-  
-    cells.forEach((cell, index) => {
-      cell.addEventListener("click", () => handleCellClick(index));
-    });
-  
-    restartButton.addEventListener("click", restartGame);
-  
-    function handleCellClick(index) {
-      if (gameBoard[index] === "" && !isGameOver()) {
-        gameBoard[index] = currentPlayer;
-        updateCell(index);
-        if (checkWinner()) {
-          alert(`${currentPlayer} is the winner!`);
-          restartGame();
-        } else if (isBoardFull()) {
-          alert("It's a draw!");
-          restartGame();
-        } else {
-          currentPlayer = currentPlayer === "X" ? "O" : "X";
-        }
-      }
-    }
-  
-    function updateCell(index) {
-      const cell = cells[index];
-      cell.textContent = gameBoard[index];
-    }
-  
-    function checkWinner() {
-        const winningCombinations = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ];
-    
-        for (const combination of winningCombinations) {
-          const [a, b, c] = combination;
-          if (
-            gameBoard[a] !== "" &&
-            gameBoard[a] === gameBoard[b] &&
-            gameBoard[a] === gameBoard[c]
-          ) {
-            return true;
-          }
-        }
-    
-        return false;
-      }
-  
-      function isBoardFull() {
-        return !gameBoard.includes("");
-      }
-  
-      function isGameOver() {
-        return checkWinner() || isBoardFull();
-      }
-  
-    function restartGame() {
-      currentPlayer = "X";
-      gameBoard = ["", "", "", "", "", "", "", "", ""];
-      cells.forEach((cell) => {
-        cell.textContent = "";
-      });
-    }
+  const cells = document.querySelectorAll(".cell");
+  const restartButton = document.querySelector(".btnRestart");
+
+  let currentPlayer = "X";
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+  let turnCount = 0;
+
+  cells.forEach((cell, index) => {
+    cell.addEventListener("click", () => handleCellClick(index));
   });
+
+  restartButton.addEventListener("click", restartGame);
+
+  function handleCellClick(index) {
+    if (gameBoard[index] === "" && !isGameOver()) {
+      gameBoard[index] = currentPlayer;
+      updateCell(index);
+      if (checkWinner()) {
+        alert(`${currentPlayer} is the winner!`);
+      } else if (isBoardFull()) {
+        alert("It's a draw!");
+      } else {
+        // Switch player for the next turn
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        turnCount++;
+      }
+    }
+  }
+
+  function updateCell(index) {
+    const cell = cells[index];
+    cell.innerHTML = "";  // Clear existing content
+
+    // Create an img element
+    const image = document.createElement("img");
+
+    image.classList.add("image");
+
+    // Set the src attribute based on the current player
+    image.src = currentPlayer === "X" ? "../assets/idoX.jpg" : "../assets/davidO.jpg";
+
+    // Set alt attribute to the current player
+    image.alt = currentPlayer;
+
+    // Append the image to the cell
+    cell.appendChild(image);
+  }
+
+  function checkWinner() {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (
+        gameBoard[a] !== "" &&
+        gameBoard[a] === gameBoard[b] &&
+        gameBoard[a] === gameBoard[c]
+      ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function isBoardFull() {
+    return !gameBoard.includes("");
+  }
+
+  function isGameOver() {
+    return checkWinner() || isBoardFull();
+  }
+
+  function restartGame() {
+    currentPlayer = turnCount % 2 === 0 ? "X" : "O";
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    cells.forEach((cell) => {
+      cell.innerHTML = "";
+    });
+    turnCount = 0;
+  }
+});
